934cf8c0c4b42b2a649133fe91886580de79e50c
cbicsabi <claudiu.bicsabi@gmail.com>
2018-03-28 23:36:58 +0300

    JavaBasics Session 2 Homework

--- src/JavaBasicsLab/MainClassForFirstSession.java
+++ src/JavaBasicsLab/MainClassForFirstSession.java
@@ -1,5 +1,7 @@
 package JavaBasicsLab;
 
+import static java.lang.Math.pow;
+
 public class MainClassForFirstSession {
 
     public static void main(String[] args) {
@@ -48,6 +50,23 @@ public class MainClassForFirstSession {
 
         printare();
 
+
+        // Homework JavaBasics Session2
+        sumTwoNumbers(1, 2);
+        subtractTwoNumbers(100, 91);
+        divideTwoNumbers(7, 3);
+        multiplyTwoNumbers(5, 7);
+
+        //V1
+        for (int i=1; i<=10; i++){
+            xToThePowerOfY(2, i);
+        }
+
+        //V2
+        xToThePowerOfYLoop(2, 10);
+
+
+
     }
 
     private static void printare() {
@@ -56,10 +75,76 @@ public class MainClassForFirstSession {
         System.out.println("Iteratia Numarul : 3");
     }
 
-    public static void printeazaValoareaLuiX(int y) {
+    private static void printeazaValoareaLuiX(int x) {
 
-        System.out.println("X este: " + y);
+        System.out.println("X este: " + x);
 
     }
+
+    private static int sumTwoNumbers(int num1, int num2){
+
+        int sum = num1 + num2;
+
+        System.out.println("Suma numerelor este: " + sum);
+
+        return sum;
+    }
+
+    private static int subtractTwoNumbers(int num1, int num2){
+
+        int difference = num1 - num2;
+
+        System.out.println("Diferenta dintre cele doua numere este: " + difference);
+
+        return difference;
+    }
+
+    private static double divideTwoNumbers(int num1, int num2){
+
+        double division = num1 / num2;
+
+        System.out.println("Impartirea numerelor este: " + division);
+
+        return division;
+    }
+
+    private static int multiplyTwoNumbers(int num1, int num2){
+
+        int multiplication = num1 * num2;
+
+        System.out.println("Inmultirea numerelor este: " + multiplication);
+
+        return multiplication;
+    }
+
+    private static int xToThePowerOfY(int x, int y){
+
+        //We are not in Python anymore, Toto...
+        //int result = x ^ y;
+
+        Double operation = pow(x, y);
+        int result = operation.intValue();
+
+        System.out.println(x + " la puterea lui " + y + " este: " + result);
+
+        return result;
+    }
+
+    private static int xToThePowerOfYLoop(int x, int y){
+
+        int result = 1;
+
+        for (int i = 0; i < y; i++) {
+            result *= x;
+        }
+
+        System.out.println(x + " la puterea lui " + y + " este: " + result);
+
+        return result;
+    }
+
+
+
+
 
 }
