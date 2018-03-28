b0ac8a6e56fc4fd4f8b69d6feb8155646d6e97d1
Claudiu Bicsabi <claudiu.bicsabi@mambu.com>
2018-03-28 18:14:08 +0300

    JavaLab Session 2

--- src/JavaBasicsLab/MainClassForFirstSession.java
+++ src/JavaBasicsLab/MainClassForFirstSession.java
@@ -3,7 +3,6 @@ package JavaBasicsLab;
 public class MainClassForFirstSession {
 
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
         int numarulMeu = 12;
         int numarulTau = 20;
 
@@ -11,11 +10,10 @@ public class MainClassForFirstSession {
         String descriere = "Suma celor doua numere este: ";
         String descriere2 = "bla bla";
         String concatenare = descriere + descriere2;
-        System.out.println(concatenare);
 
-        System.out.println("Iteratia Numarul : 1");
-        System.out.println("Iteratia Numarul : 2");
-        System.out.println("Iteratia Numarul : 3");
+        System.out.println(concatenare);
+        System.out.println(suma);
+        printare();
 
         for (int i = 0; i <= 1000; i=i+100) {
 
@@ -26,6 +24,8 @@ public class MainClassForFirstSession {
         int x = 0;    //initializare
 
         while (x < 6) {
+
+            printeazaValoareaLuiX(x);
 
             int restulImpartirii = x % 2;
 
@@ -39,9 +39,26 @@ public class MainClassForFirstSession {
                     System.out.println("Am intrat pe ramura 2 de IF : " + x);
                 }
             }
+
+            System.out.println("--------------");
+
             x++;
 
         }
+
+        printare();
+
+    }
+
+    private static void printare() {
+        System.out.println("Iteratia Numarul : 1");
+        System.out.println("Iteratia Numarul : 2");
+        System.out.println("Iteratia Numarul : 3");
+    }
+
+    public static void printeazaValoareaLuiX(int y) {
+
+        System.out.println("X este: " + y);
 
     }
 
